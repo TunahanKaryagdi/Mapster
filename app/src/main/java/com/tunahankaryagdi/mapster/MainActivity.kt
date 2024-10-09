94b6f8f580af36182c70541592be5e9db1cf5a53
Tunahan Karyağdı <thankaryagdi@gmail.com>
2024-10-09 15:12:38 +0300

    git init

---
+++ app/src/main/java/com/tunahankaryagdi/mapster/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tunahankaryagdi.mapster
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tunahankaryagdi.mapster.ui.theme.MapsterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MapsterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MapsterTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/tunahankaryagdi/mapster/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.tunahankaryagdi.mapster.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val Orange = Color(0xFFFFAB1A)
+val Grey = Color(0xFFF5F0E5)
+val Black = Color(0xFF1C170D)
+val White = Color(0xFFFFFFFF)
